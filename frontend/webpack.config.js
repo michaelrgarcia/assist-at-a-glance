c626a1acdb0a66edf45f02ee6edeacc30840fd23
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-21 14:45:39 -0700

    Initial setup

---
+++ frontend/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "imgs/[name].[ext]",
+        assetModuleFilename: "svgs/[name].[ext]",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+};
