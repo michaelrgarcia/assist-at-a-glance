fccbc8302507bfe32fa773c5c8a6c71a0d3087d0
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-21 17:28:25 -0700

    Use static middleware to render index

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from "express";
-import path from "path";
-
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const port = 10000;
 
+app.use(express.static("../frontend/dist/"));
+
+/*  
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
+
+*/
 
 app.listen(port);
