5d5f4e66d0b7126e2b20c55973ab7761597c89d3
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-21 17:57:32 -0700

    Import node.js code from convenient-assist

--- backend/routes/articulations.js
+++ backend/routes/articulations.js
@@ -8,3 +8,87 @@ router.get("/:year/:sending/:receiving/:key", (req, res) => {
 });
 
 module.exports = router;
+
+/*
+
+import { alphaSort, deNest, jsonFromServer } from "./utilities.js";
+
+async function getArticulationData(year, sending, receiving, key) {
+  const articulationPage = `https://assist.org/api/articulation/Agreements?Key=${year}/${sending}/to/${receiving}/Major/${key}`;
+
+  const json = jsonFromServer(articulationPage);
+  const articulationData = Object.values(json)[0];
+
+  return articulationData;
+}
+
+function getLowerDivList(articulationData) {
+// to continue, paste raw templateAssets into https://jsonviewer.stack.hu/
+  const lowerDivs = deNest(articulationData.templateAssets);
+  const classList = [];
+
+  lowerDivs.forEach((obj) => {
+    if (obj.type === "RequirementGroup") {
+      const {sections} = obj;
+      sections.forEach((section) => {
+        section.rows.forEach((row) => {
+          row.cells.forEach((cell) => {
+            if (cell.course) {
+              const {course} = cell;
+
+              const {prefix, courseNumber, courseTitle} = course;
+
+              classList.push({ prefix, courseNumber, courseTitle });
+            }
+          });
+        });
+      });
+    }
+  });
+
+  return alphaSort(classList, "prefix");;
+
+  // handle series (use irvine cs reqs as the url?)
+}
+
+export default async function organizeArticulations(articulationData) {
+// to continue, paste raw templateAssets into https://jsonviewer.stack.hu/
+  const availableArticulations = deNest(articulationData.articulations);
+
+  availableArticulations.forEach((obj) => {
+    const uniCourse = obj.articulation.course
+    const ccCourse = obj.articulation.sendingArticulation;
+    
+    code block below is commented out
+
+    if (obj.articulation.course) {
+      const {prefix, courseNumber, courseTitle} = uniCourse
+      const receiving = {prefix, courseNumber, courseTitle}
+
+      const coursePair = {receiving};
+      console.log(coursePair);
+    }
+
+    
+
+    
+    
+    // for each obj.articulation ...
+  });
+
+  return availableArticulations;
+  // handle series
+}
+
+const eecsData = await getArticulationData(
+  74,
+  17,
+  79,
+  "9df99d8d-2540-4677-891f-a559d4801840",
+);
+
+ console.log(eecsData);
+organizeArticulations(eecsData);
+ console.log(getLowerDivList(eecsData));
+
+*/
--- backend/routes/schools.js
+++ backend/routes/schools.js
@@ -16,3 +16,68 @@ router.get("/major-data/:receiving/:sending/:year", (req, res) => {
 });
 
 module.exports = router;
+
+/*
+
+import { alphaSort, jsonFromServer } from "./utilities.js";
+
+export function getMajorData(receiving, sending, year) {
+  const majorKeysByUni = `https://assist.org/api/agreements?receivingInstitutionId=${receiving}&sendingInstitutionId=${sending}&academicYearId=${year}&categoryCode=major`;
+
+  const json = jsonFromServer(majorKeysByUni);
+  const majors = Object.values(json); // array
+
+  const majorData = [];
+
+  majors[0].forEach((dataset) => {
+    const major = dataset.label;
+    const keyStr = dataset.key;
+    const key = keyStr.split("/").pop();
+
+    majorData.push({ major, key });
+  });
+
+  return alphaSort(majorData, "major");
+}
+
+export async function getSendingSchools() {
+  const json = await fetch("http://localhost:80/schoolList", {mode: "cors"});
+  const schoolData = Object.values(json); // array
+
+  const schoolList = [];
+
+  schoolData.forEach((school) => {
+    if (school.isCommunityCollege) {
+      const nameList = school.names;
+      const { name } = nameList[nameList.length - 1];
+      const { id } = school;
+
+      schoolList.push({ name, id });
+    }
+  });
+
+  return alphaSort(schoolList, "name");
+}
+
+export function getReceivingSchools() {
+  const institutions = "https://assist.org/api/institutions";
+
+  const json = jsonFromServer(institutions);
+  const schoolData = Object.values(json); // array
+
+  const schoolList = [];
+
+  schoolData.forEach((school) => {
+    if (!school.isCommunityCollege) {
+      const nameList = school.names;
+      const { name } = nameList[nameList.length - 1];
+      const { id } = school;
+
+      schoolList.push({ name, id });
+    }
+  });
+
+  return alphaSort(schoolList, "name");
+}
+
+*/
