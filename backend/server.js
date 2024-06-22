94ee13edefb6a23ee72e3b6b6840285bc4e91c46
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-21 17:06:26 -0700

    Import path + var names for node

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from "express";
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 10000;
 
 app.get("/", (req, res) => {
-  console.log("yo");
-  res.send("hello");
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 app.listen(port);
