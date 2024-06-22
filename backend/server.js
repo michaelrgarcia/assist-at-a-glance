e266ae04ef0ee76d21f1fffd9a6f9551f7b915da
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-22 11:54:49 -0700

    Dummy commit

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,4 @@ const articulationRouter = require("./routes/articulations.js");
 app.use("/schools", schoolRouter);
 app.use("/articulations", articulationRouter);
 
-/*  
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
-*/
-
 app.listen(port);
