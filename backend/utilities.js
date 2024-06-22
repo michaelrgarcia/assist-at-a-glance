5a659802626065ba109b93596d6c9f332d52ca6f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-21 18:33:52 -0700

    Export getJson, alphaSort, and deNest

---
+++ backend/utilities.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+
+const _ = require("lodash");
+const puppeteer = require("puppeteer");
+
+async function getJson(link) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(link);
+
+  const pre = await page.$eval("pre", (element) => element.textContent);
+  const parsedPre = JSON.parse(pre);
+
+  await browser.close();
+
+  return parsedPre;
+}
+
+function alphaSort(array, str) {
+  let arr = array;
+  arr = _.orderBy(arr, [str], ["asc"]);
+
+  return arr;
+}
+
+function deNest(data) {
+  const json = JSON.parse(data);
+  const processed = Object.values(json);
+
+  return processed;
+}
+
+module.exports = { getJson, alphaSort, deNest };
