4fdc9e551ab4429efdfd348d988a40479f93f967
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-21 17:52:42 -0700

    Remove routers from schools.js

--- backend/routes/articulations.js
+++ backend/routes/articulations.js
@@ -3,16 +3,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/articulationData/:year/:sending/:receiving/:key", (req, res) => {
+router.get("/:year/:sending/:receiving/:key", (req, res) => {
   res.send("Will send JSON from getArticulationData()");
-});
-
-router.get("/four-years", (req, res) => {
-  res.send("Will send JSON from getSendingSchools()");
-});
-
-router.get("/major-data/:receiving/:sending/:year", (req, res) => {
-  res.send("Will send JSON from getMajorData()");
 });
 
 module.exports = router;
