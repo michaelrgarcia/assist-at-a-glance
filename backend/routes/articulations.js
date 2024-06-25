405a69227242fa9470c8da3173f75b7d3facc8eb
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-24 18:31:46 -0700

    Finish createGroup, extractGroupConnector, and getSendingCourses (series not done)

--- backend/routes/articulations.js
+++ backend/routes/articulations.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 const express = require("express");
-const { getJson, deNest, alphaSort } = require("../utilities.js");
+const { getJson, deNest, alphaSort, conjoin } = require("../utilities.js");
 
 const router = express.Router();
 
@@ -67,6 +67,9 @@ router.get("/:year/:sending/:receiving/:key/lower-divs", async (req, res) => {
 });
 
 router.get("/:year/:sending/:receiving/:key/articulated", async (req, res) => {
+  // will remove "articulated" in url when done
+  // when done, paste this code into the top route
+
   const year = req.params.year;
   const sending = req.params.sending;
   const receiving = req.params.receiving;
@@ -79,18 +82,25 @@ router.get("/:year/:sending/:receiving/:key/articulated", async (req, res) => {
     key
   );
 
-  // to continue, paste raw templateAssets into https://jsonviewer.stack.hu/
-
   const availableArticulations = deNest(articulationData.articulations);
 
-  // let classList = [];
+  let articulationGroup = [];
+
+  availableArticulations.forEach((dataset) => {
+    const articulationObj = dataset.articulation;
+
+    const receiving = getReceivingCourses(articulationObj);
+    const sending = getSendingCourses(articulationObj);
+
+    articulationGroup.push({ receiving, sending });
+  });
 
   // classList = alphaSort(classList, "prefix");
 
-  res.json(availableArticulations);
-
-  // handle series (use irvine cs reqs as the url?)
+  res.json(articulationGroup);
 });
+
+//move below functions to utilities
 
 async function getArticulationData(year, sending, receiving, key) {
   const articulationPage = `https://assist.org/api/articulation/Agreements?Key=${year}/${sending}/to/${receiving}/Major/${key}`;
@@ -101,42 +111,92 @@ async function getArticulationData(year, sending, receiving, key) {
   return articulationData;
 }
 
+function getReceivingCourses(articulationObj) {
+  if (articulationObj.course) {
+    const courseObj = articulationObj.course;
+
+    return getCourse(courseObj);
+  } else if (articulationObj.series) {
+    // const seriesObj = articulationObj.series;
+  }
+}
+
+function getSendingCourses(articulationObj) {
+  //handle series...
+  const sendingArticulation = articulationObj.sendingArticulation;
+  const items = sendingArticulation.items;
+
+  let courseList = [];
+
+  items.forEach((courseObj) => {
+    const courses = courseObj.items;
+
+    if (courses.length > 1) {
+      const connector = courseObj.courseConjunction;
+      let courseGroup = createGroup(connector, courses);
+
+      courseList.push(courseGroup);
+    } else {
+      const course = getCourse(courses[0]);
+
+      courseList.push(course);
+    }
+  });
+
+  if (items.length > 1) {
+    const groupConnector = extractGroupConnector(sendingArticulation);
+
+    return conjoin(courseList, groupConnector);
+  } else {
+    return courseList;
+  }
+}
+
+// for lowerDiv function
+function seriesBreakdown() {}
+
+function extractGroupConnector(sendingArticulation) {
+  const arr = sendingArticulation.courseGroupConjunctions;
+  const lastItem = arr[arr.length - 1];
+
+  return lastItem.groupConjunction;
+}
+
+function getCourse(courseObj) {
+  const { prefix, courseNumber, courseTitle } = courseObj;
+
+  return { prefix, courseNumber, courseTitle };
+}
+
+function createGroup(conjunction, groupCourses) {
+  let group = [];
+  const connector = conjunction;
+  const coursesInGroup = groupCourses;
+
+  // may not need these...
+  const pad1 = connector.padStart(connector.length + 1, " ");
+  const pad2 = pad1.padEnd(pad1.length + 1, " ");
+
+  coursesInGroup.forEach((courseObj) => {
+    const course = getCourse(courseObj);
+
+    group.push(course);
+  });
+
+  group = alphaSort(group, "courseNumber");
+
+  return conjoin(group, pad2);
+}
+
+//series function checks for articulation.type == "series"
+
 module.exports = router;
 
 // berkeley eecs major key: 9df99d8d-2540-4677-891f-a559d4801840
 
-// sending: 113, receiving: 79, year: 74
+// year: 74, sending: 113, receiving: 79
 
 /*
-
-export default async function organizeArticulations(articulationData) {
-// to continue, paste raw templateAssets into https://jsonviewer.stack.hu/
-  const availableArticulations = deNest(articulationData.articulations);
-
-  availableArticulations.forEach((obj) => {
-    const uniCourse = obj.articulation.course
-    const ccCourse = obj.articulation.sendingArticulation;
-    
-    code block below is commented out
-
-    if (obj.articulation.course) {
-      const {prefix, courseNumber, courseTitle} = uniCourse
-      const receiving = {prefix, courseNumber, courseTitle}
-
-      const coursePair = {receiving};
-      console.log(coursePair);
-    }
-
-    
-
-    
-    
-    // for each obj.articulation ...
-  });
-
-  return availableArticulations;
-  // handle series
-}
 
 const eecsData = await getArticulationData(
   74,
