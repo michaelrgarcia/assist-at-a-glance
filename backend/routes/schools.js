bd6552bfc0b47e6a4d799bfe733f9f428388e084
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-26 23:10:37 -0700

    Move getLowerDivs, seriesBreakdown to schools.js

--- backend/routes/schools.js
+++ backend/routes/schools.js
@@ -5,6 +5,7 @@ const {
   getCommunityColleges,
   getFourYears,
   getMajorData,
+  getLowerDivs,
 } = require("../public/schoolTools.js");
 
 const router = express.Router();
@@ -29,6 +30,50 @@ router.get("/major-data/:receiving/:sending/:year", async (req, res) => {
   const majorData = await getMajorData(receiving, sending, year);
 
   res.json(majorData);
+});
+
+router.get("/:year/:sending/:receiving/:key/lower-divs", async (req, res) => {
+  const year = req.params.year;
+  const sending = req.params.sending;
+  const receiving = req.params.receiving;
+  const key = req.params.key;
+
+  const articulationData = await getArticulationData(
+    year,
+    sending,
+    receiving,
+    key
+  );
+
+  // to continue, paste raw templateAssets into https://jsonviewer.stack.hu/
+
+  const lowerDivs = deNest(articulationData.templateAssets);
+  let classList = [];
+
+  lowerDivs.forEach((obj) => {
+    if (obj.type === "RequirementGroup") {
+      const { sections } = obj;
+      sections.forEach((section) => {
+        section.rows.forEach((row) => {
+          row.cells.forEach((cell) => {
+            if (cell.course) {
+              const { course } = cell;
+
+              const { prefix, courseNumber, courseTitle } = course;
+
+              classList.push({ prefix, courseNumber, courseTitle });
+            } else if (cell.series) {
+              // series function goes here. pass in the series obj
+            }
+          });
+        });
+      });
+    }
+  });
+
+  classList = alphaSort(classList, "prefix");
+
+  res.json(classList);
 });
 
 module.exports = router;
