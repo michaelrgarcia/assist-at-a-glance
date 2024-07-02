59d8cf9278c8b6d616bd8d44b4c314c234371f3e
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-01 23:31:28 -0700

    Enable CORS

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 10000;
 
 const schoolRouter = require("./routes/schools.js");
 const articulationRouter = require("./routes/articulations.js");
+
+app.use(cors());
 
 app.use("/schools", schoolRouter);
 app.use("/articulations", articulationRouter);
