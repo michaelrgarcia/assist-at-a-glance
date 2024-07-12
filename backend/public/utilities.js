9ba02fc49ab453b45edc2f4dbbd3be5b88bc74a9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-12 01:25:22 -0700

    Attempt performance boost of getJson

--- backend/public/utilities.js
+++ backend/public/utilities.js
@@ -3,13 +3,53 @@
 const _ = require("lodash");
 const puppeteer = require("puppeteer");
 
+const minimal_args = [
+  "--autoplay-policy=user-gesture-required",
+  "--disable-background-networking",
+  "--disable-background-timer-throttling",
+  "--disable-backgrounding-occluded-windows",
+  "--disable-breakpad",
+  "--disable-client-side-phishing-detection",
+  "--disable-component-update",
+  "--disable-default-apps",
+  "--disable-dev-shm-usage",
+  "--disable-domain-reliability",
+  "--disable-extensions",
+  "--disable-features=AudioServiceOutOfProcess",
+  "--disable-hang-monitor",
+  "--disable-ipc-flooding-protection",
+  "--disable-notifications",
+  "--disable-offer-store-unmasked-wallet-cards",
+  "--disable-popup-blocking",
+  "--disable-print-preview",
+  "--disable-prompt-on-repost",
+  "--disable-renderer-backgrounding",
+  "--disable-setuid-sandbox",
+  "--disable-speech-api",
+  "--disable-sync",
+  "--hide-scrollbars",
+  "--ignore-gpu-blacklist",
+  "--metrics-recording-only",
+  "--mute-audio",
+  "--no-default-browser-check",
+  "--no-first-run",
+  "--no-pings",
+  "--no-sandbox",
+  "--no-zygote",
+  "--password-store=basic",
+  "--use-gl=swiftshader",
+  "--use-mock-keychain",
+];
+
 async function getJson(link) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto(link);
+  await page.goto(link, { waitUntil: "networkidle2", timeout: 30000 });
 
-  const pre = await page.$eval("pre", (element) => element.textContent);
-  const parsedPre = JSON.parse(pre);
+  const parsedPre = await page.evaluate(() => {
+    const pre = document.querySelector("pre");
+    return pre ? JSON.parse(pre.textContent) : null;
+  });
 
   await browser.close();
 
