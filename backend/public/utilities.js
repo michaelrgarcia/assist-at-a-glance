b73b2e160d513faa0edc02fe54a20188818c5563
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-07-13 23:13:32 -0700

    Remove minimal args for puppeteer
    Going to delegate puppeteer work to AWS lambda.

--- backend/public/utilities.js
+++ backend/public/utilities.js
@@ -3,44 +3,6 @@
 const _ = require("lodash");
 const puppeteer = require("puppeteer");
 
-const minimal_args = [
-  "--autoplay-policy=user-gesture-required",
-  "--disable-background-networking",
-  "--disable-background-timer-throttling",
-  "--disable-backgrounding-occluded-windows",
-  "--disable-breakpad",
-  "--disable-client-side-phishing-detection",
-  "--disable-component-update",
-  "--disable-default-apps",
-  "--disable-dev-shm-usage",
-  "--disable-domain-reliability",
-  "--disable-extensions",
-  "--disable-features=AudioServiceOutOfProcess",
-  "--disable-hang-monitor",
-  "--disable-ipc-flooding-protection",
-  "--disable-notifications",
-  "--disable-offer-store-unmasked-wallet-cards",
-  "--disable-popup-blocking",
-  "--disable-print-preview",
-  "--disable-prompt-on-repost",
-  "--disable-renderer-backgrounding",
-  "--disable-setuid-sandbox",
-  "--disable-speech-api",
-  "--disable-sync",
-  "--hide-scrollbars",
-  "--ignore-gpu-blacklist",
-  "--metrics-recording-only",
-  "--mute-audio",
-  "--no-default-browser-check",
-  "--no-first-run",
-  "--no-pings",
-  "--no-sandbox",
-  "--no-zygote",
-  "--password-store=basic",
-  "--use-gl=swiftshader",
-  "--use-mock-keychain",
-];
-
 async function getJson(link) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
